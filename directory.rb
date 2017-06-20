f84429c175eb90848daa051483fbc4efe4dd25b5
James Ransome <jamesransome@msn.com>
2017-06-20 16:54:38 +0100

    added hobbies, height and country of birth to database

--- directory.rb
+++ directory.rb
@@ -6,9 +6,7 @@ end
 def print(students, starting_letter)
   i = 0
   while i < students.length
-    if students[i][:name][0].downcase == starting_letter.downcase && students[i][:name].length < 12
-      puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
-    end
+    puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort), birthplace: #{students[i][:country]}, height: #{students[i][:height]}cm, favourite hobby: #{students[i][:hobby]}."
     i += 1
   end
 end
@@ -18,8 +16,8 @@ def print_footer(students)
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the student's first name"
+  puts "To quit instead, hit return without typing"
   #create an empty array
   students = []
   # get the first names
@@ -27,9 +25,17 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    puts "What is #{name}'s favourite hobby?"
+    hobby = gets.chomp
+    puts "What is #{name}'s height in cm?"
+    height = gets.chomp
+    puts "What is #{name}'s country of birth?"
+    country = gets.chomp
+    students << {name: name, hobby: hobby, height: height, country: country, cohort: :november}
     puts "Now we have #{students.count} students"
     # get another name from the user
+    puts "Please enter the next student's first name"
+    puts "If you've finished adding students, hit return without typing instead"
     name = gets.chomp
   end
   # return array of students
