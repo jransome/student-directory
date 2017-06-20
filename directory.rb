6b3b0568d89c0cf1c4ec9f8279ab83bb566ce6ea
James Ransome <jamesransome@msn.com>
2017-06-20 14:02:16 +0100

    refactored directory.rb to use methods

--- directory.rb
+++ directory.rb
@@ -12,13 +12,23 @@ students = [
   "Joffery Baratheon",
   "Norman Bates"
 ]
-#then print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
 
-#finally print the total number of students
-puts "Overall, we have #{students.count} great students"
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#nothing happens untill we call these methods
+print_header
+print(students)
+print_footer(students)
