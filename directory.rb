a1680173ef3ceddbf041084701b8dd5451405529
James Ransome <jamesransome@msn.com>
2017-06-21 18:45:19 +0100

    refactored interactive_menu method (step 10)

--- directory.rb
+++ directory.rb
@@ -1,33 +1,47 @@
+@students = [] # empty array accessible to all methods
 $line_width = 100
 $default_cohort = :November
 $months = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
+
 
 def interactive_menu
   students = []
   loop do
     # print the menu of options
-    puts "1. Input new students"
-    puts "2. Show existing students"
-    puts "9. Exit"
+    print_menu
     # read input and save to a variable
     selection = gets.chomp
     # do what the user has asked
-    case selection
-      when "1"
-        # input students
-        students = input_students
-      when "2"
-        # show students
-        print_header
-        print_directory(students)
-        print_footer(students)
-      when "9"
-        # exit program
-        exit
-      else
-        puts "I don't know what you meant, please try again"
-    end
+    process(selection)
   end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      # input students
+      input_students
+    when "2"
+      # show students
+      show_students
+    when "9"
+      # exit program
+      exit
+    else
+      puts "I don't know what you meant, please try again"
+  end
+end
+
+def print_menu
+  puts "1. Input new students"
+  puts "2. Show existing students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def print_header
@@ -35,31 +49,29 @@ def print_header
   puts "-------------".center($line_width)
 end
 
-def print_directory(students)
-  if students.empty?
+def print_students_list
+  if @students.empty?
     puts "No students in database!"
     return
   end
-  students.sort_by! {|s| s[:cohort] }
-  students.each_with_index do | student, i |
+  @students.sort_by! {|s| s[:cohort] }
+  @students.each_with_index do | student, i |
     puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort), birthplace: #{student[:country]}, height: #{student[:height]}cm, favourite hobby: #{student[:hobby]}.".ljust($line_width)
   end
 end
 
-def print_footer(students)
-  if students.empty?
+def print_footer
+  if @students.empty?
     puts "No students in database!"
     return
   end
-  print "Overall, we have #{students.count} great "
-  puts students.count == 1 ? "student" : "students"
+  print "Overall, we have #{@students.count} great "
+  puts @students.count == 1 ? "student" : "students"
 end
 
 def input_students
   puts "Please enter the student's first name"
   puts "To quit instead, hit return without typing"
-  #create an empty array
-  students = []
   # get the first names
   name = gets.gsub(/\n/, "")
   # while the name is not empty, repeat this code
@@ -78,21 +90,15 @@ def input_students
     height = gets.gsub(/\n/, "")
     puts "What is #{name}'s country of birth?"
     country = gets.gsub(/\n/, "")
-    students << {name: name, hobby: hobby, height: height, country: country, cohort: cohort}
-    print "Now we have #{students.count} "
-    puts students.count == 1 ? "student" : "students"
+    @students << {name: name, hobby: hobby, height: height, country: country, cohort: cohort}
+    print "Now we have #{@students.count} "
+    puts @students.count == 1 ? "student" : "students"
     # get another name from the user
     puts "Please enter the next student's first name"
     puts "If you've finished adding students, hit return without typing instead"
     name = gets.gsub(/\n/, "")
   end
-  # return array of students
-  students
 end
 
 #nothing happens untill we call these methods
 interactive_menu
-#students = input_students
-#print_header
-#print_directory(students)
-#print_footer(students)
