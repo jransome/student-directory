2069b45be07254506a7f495c3b272dc2914fdb68
James Ransome <jamesransome@msn.com>
2017-06-21 11:08:52 +0100

    'student' is now correctly pluralised when printed (step 8.9)

--- directory.rb
+++ directory.rb
@@ -5,7 +5,7 @@ def print_header
   puts "-------------".center($line_width)
 end
 
-def print(students)
+def print_directory(students)
   students.sort_by! {|s| s[:cohort] }
   students.each_with_index do | student, i |
     puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort), birthplace: #{student[:country]}, height: #{student[:height]}cm, favourite hobby: #{student[:hobby]}.".ljust($line_width)
@@ -13,7 +13,8 @@ def print(students)
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  print "Overall, we have #{students.count} great "
+  puts students.count == 1 ? "student" : "students"
 end
 
 def input_students
@@ -28,7 +29,6 @@ def input_students
     puts "What cohort is #{name} in?"
     cohort = gets.chomp
     cohort == "" ? cohort = $default_cohort : cohort = cohort.to_sym
-    puts cohort.inspect
     puts "What is #{name}'s favourite hobby?"
     hobby = gets.chomp
     puts "What is #{name}'s height in cm?"
@@ -36,7 +36,8 @@ def input_students
     puts "What is #{name}'s country of birth?"
     country = gets.chomp
     students << {name: name, hobby: hobby, height: height, country: country, cohort: cohort}
-    puts "Now we have #{students.count} students"
+    print "Now we have #{students.count} "
+    puts students.count == 1 ? "student" : "students"
     # get another name from the user
     puts "Please enter the next student's first name"
     puts "If you've finished adding students, hit return without typing instead"
@@ -49,5 +50,5 @@ end
 #nothing happens untill we call these methods
 students = input_students
 print_header
-print(students)
+print_directory(students)
 print_footer(students)
