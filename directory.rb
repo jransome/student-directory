8a443d6b4912b069870ebdba2840e6574e39908d
James Ransome <jamesransome@msn.com>
2017-06-22 15:50:49 +0100

    opens students.csv if no argument passed from command line (step 14.2)

--- directory.rb
+++ directory.rb
@@ -26,7 +26,10 @@ end
 
 def try_load_students
   filename = ARGV.first # first argument from the command line
-  return if filename.nil?
+  if filename.nil?
+    load_students
+    return
+  end
   if File.exists?(filename)
     load_students(filename)
     puts "Loaded #{@students.count} records from #{filename}"
